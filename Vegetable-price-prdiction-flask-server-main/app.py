c8f596a42a6cbe17aae8e88483025e166bc0fa48
Zeeshan Khan <zeeshankhn00786@gmail.com>
2023-09-21 22:41:15 +0530

    commit 1

---
+++ Vegetable-price-prdiction-flask-server-main/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import model
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def sayHi():
+    return "HI"
+
+@app.route('/predict', methods=['GET'])
+def predictPrice():
+    res = model.predict(request.args.get('name'))
+    #res = model.predict('Amla')
+    return jsonify(res)
+
+
+@app.route('/getprice', methods=['GET'])
+def predict():
+    res = model.getPrice(request.args.get('name'))
+    return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.9", port=8080, debug=True)
